7014b50937694d2f3ba12edbc1d92129e4fd67bb
Nadezhda Chornaya <nadezhdachornaja@gmail.com>
2021-04-14 13:21:45 +0300

    added mail ser

--- model/email.js
+++ model/email.js
@@ -1,18 +1,44 @@
 const sgMail = require('@sendgrid/mail')
-
+const Mailgen = require('mailgen')
 require('dotenv').config()
 
 const { SENDGRID_API_KEY, EMAIL_SENDER } = process.env
 
-const sendMail = async (verifyToken, email = 'nadezhdachornaya188@gmail.com') => {
+const mailGenerator = new Mailgen({
+    theme: 'default',
+    product: {
+        name: 'Developer of this server',
+        link: 'https://localhost:3000/',
+    },
+})
+
+const sendMail = async (verifyToken, email) => {
     sgMail.setApiKey(SENDGRID_API_KEY)
+
+    const template = {
+        body: {
+            name: email,
+            intro: 'Email verification needed',
+            action: {
+                instructions: 'To complete the registration process please press the button:',
+                button: {
+                    color: '#22BC66', // Optional action button color
+                    text: 'Confirm email',
+                    link: `http://localhost:3000/api/users/verify/${verifyToken}`,
+                },
+            },
+            outro: "Need help, or have questions? Please figure it out yourselves, we can't be bothered to help you",
+        },
+    }
+
+    const verificationMail = mailGenerator.generate(template)
 
     const msg = {
         to: email,
         from: EMAIL_SENDER,
         subject: 'Sending with SendGrid is Fun',
         text: 'Hello! We are testing email seding',
-        html: '<strong>Hello! We are testing email seding</strong>',
+        html: verificationMail,
     }
 
     sgMail.send(msg)
